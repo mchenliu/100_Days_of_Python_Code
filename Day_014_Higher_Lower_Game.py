7f333b98144e94fbf7aa90b6775433054f53ec73
mchenliu <meichenliu83@gmail.com>
2024-08-30 10:58:09 +0800

    Update Day_014_Higher_Lower_Game.py

--- Day_014_Higher_Lower_Game.py
+++ Day_014_Higher_Lower_Game.py
@@ -1,25 +1,28 @@
 import random
+import sys, os
 from Day_014_Higher_Lower_Game_Art import logo, vs
 from Day_014_Higher_Lower_Game_Data import data
 
-score = 0
 def pick_one():
     pick_one = random.choice(data)
     return pick_one.get('name'),pick_one.get('description'),pick_one.get('follower_count'),pick_one.get('country')
+
 def answer_incorrect():
     global score
-    print('\n'*50)
+    os.system('cls')
     print(logo)
-    print(f"Sorry, that's wrong. Final score: {score}.") 
+    print(f"Sorry, that's wrong. Final score: {score}.", end='\r') 
+
 def answer_correct():
     global score
     score += 1
+    
         
     
 def game():
     nameA, descriptionA, follower_countA, countryA = pick_one()
     nameB, descriptionB, follower_countB, countryB = pick_one()
-    print(logo)
+    
     print(f'Compare A: {nameA}, a {descriptionA}, from {countryA}')
     print(vs)
     print(f'Against B: {nameB}, a {descriptionB}, from {countryB}')
@@ -33,12 +36,11 @@ def game():
         user_answer = input("Who has more followers? Type 'A' or 'B': ").upper()
         if user_answer == correct_answer:
             answer_correct()
-            print('\n'*50)
-            print(logo)
             nameA = nameB
             descriptionA = descriptionB
             countryA = countryB
-            
+            os.system('cls')
+            print(logo)
             print(f"You're right! Current score: {score}.")
             print(f'Compare A: {nameA}, a {descriptionA}, from {countryA}')
             pick_one()
@@ -50,7 +52,17 @@ def game():
         else:
             answer_incorrect()
             game_over = True
+
+score = 0
+print(logo)  
+
 game()           
+
+
+
+
+
+
 
 
 
